cf84e7ef4f80ffe84dd55ba394652e2d8c9e9d5e
Jacob Seibert <jacob.seibert@enway.ai>
2020-01-13 13:41:23 +0800

    Add missing headers to repo

---
+++ gazebo/rendering/GpuLaserCameraSetting.hh
@@ -0,0 +1,22 @@
+//
+// Created by jacob on 17.12.19.
+//
+
+#ifndef GAZEBO_GPULASERCAMERASETTING_HH_
+#define GAZEBO_GPULASERCAMERASETTING_HH_
+
+namespace gazebo
+{
+
+namespace rendering
+{
+  class GpuLaserCameraSetting
+  {
+  public: double azimuthOffset;
+  public: double elevationOffset;
+  };
+}
+
+}
+
+#endif //GAZEBO_GPULASERCAMERASETTING_HH_
